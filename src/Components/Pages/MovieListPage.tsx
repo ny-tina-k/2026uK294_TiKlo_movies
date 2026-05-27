430f15040228328d37b6a42c264b39c501fc5539
ny-tina-k <tina.klomp@noseryoung.ch>
2026-05-27 09:23:38 +0200

    implement MovieListPage and service

--- src/Components/Pages/MovieListPage.tsx
+++ src/Components/Pages/MovieListPage.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, List, ListItem, ListItemText } from '@mui/material';
+import { getMovies, deleteMovie } from '../../Services/movieService';
+import type { Movie } from '../../Services/movieService';
 
 const MovieListPage = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getMovies().then(setMovies);
+    }, []);
+
+    const handleDelete = async (id: string) => {
+        await deleteMovie(id);
+        setMovies(movies.filter(m => m.id !== id));
+    };
+
     return (
-        <div>MovieListPage</div>
+        <List>
+            {movies.map(movie => (
+                <ListItem key={movie.id}>
+                    <ListItemText primary={movie.title} secondary={movie.releaseDate} />
+                    <Button onClick={() => navigate(`/movies/${movie.id}`)}>Detail</Button>
+                    <Button onClick={() => handleDelete(movie.id)}>Delete</Button>
+                </ListItem>
+            ))}
+        </List>
     );
 };
 
--- src/Services/movieService.ts
+++ src/Services/movieService.ts
@@ -3,6 +3,12 @@ import type { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from "axio
 
 const BASE_URL = 'http://localhost:3030';
 
+export interface Movie {
+    id: string;
+    title: string;
+    releaseDate: string;
+}
+
 export const baseInstance: AxiosInstance = axios.create({
     baseURL: BASE_URL,
 })
@@ -24,4 +30,29 @@ baseInstance.interceptors.request.use((config: InternalAxiosRequestConfig<any>) 
 export const login = async (email: string, password: string) => {
   const response = await axios.post(`${BASE_URL}/login`, { email, password });
   return response.data;
+};
+
+export const getMovies = async () => {
+    const response = await baseInstance.get('movies');
+    return response.data;
+};
+
+export const getMovieById = async (id: string) => {
+    const response = await baseInstance.get(`movies/${id}`);
+    return response.data;
+};
+
+export const createMovie = async (movie: Omit<Movie, 'id'>) => {
+    const response = await baseInstance.post('movies', movie);
+    return response.data;
+};
+
+export const updateMovie = async (id: string, movie: Partial<Movie>) => {
+    const response = await baseInstance.put(`movies/${id}`, movie);
+    return response.data;
+};
+
+export const deleteMovie = async (id: string) => {
+    const response = await baseInstance.delete(`movies/${id}`);
+    return response.data;
 };
